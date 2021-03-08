050dd16b4b6cfce6777417c8f6aa56d4a4e12581
yanj <2744915954@qq.com>
2021-03-08 20:21:59 +0800

    添加一个test

---
+++ git-test/src/test/java/com/yanj/Test.java
@@ -0,0 +1,11 @@
+package com.yanj;
+
+public class Test {
+
+    @org.junit.Test
+    public void test1(){
+        System.out.println("今天天气这号");
+        System.out.println("今天天气这号");
+        System.out.println("今天天气正好");
+    }
+}
